74ab297b3b3f384b924cd787a2fa7e9647f18fde
NoTree277 <poom271978@hotmail.com>
2020-05-12 10:51:12 +0700

    Exercise4

--- Assignments/Exercise4_Poompitak_P.py
+++ Assignments/Exercise4_Poompitak_P.py
@@ -1,1 +1,9 @@
-print("he")
+FE  = 76
+GB  = 79
+ICM = 86
+CP  = 93
+print("---Youre Score---")
+print("Foundation English-----------------",FE)
+print("General Business-------------------",GB)
+print("Introduction to Computer Systems---",ICM)
+print("Computer Programming---------------",CP)
